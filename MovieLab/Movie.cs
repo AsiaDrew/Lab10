d2cc1a3e6db9d756c3685045115f95864fd8896a
AsiaDrew <asiadrew2@outlook.com>
2022-07-07 09:22:11 -0400

    first commit

---
+++ MovieLab/Movie.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLab
+{
+    internal class Movie
+    {
+       public string title { get; set; }
+		public string category { get; set; }
+
+		public Movie(string titles, string categories)
+		{
+			this.title = titles;
+			this.category = categories;
+		}
+
+		public void MovieList(List<Movie> movies, string category)
+		{
+
+            Console.WriteLine();
+            Console.WriteLine($"You picked {category}!");
+            Console.WriteLine();
+			Console.WriteLine($"List of {category} movies are:");
+			Console.WriteLine(" == == == == == == == ==  ");
+
+
+			for (int i = 0; i <= movies.Count - 1; i++)
+			{
+				if (category == "Animated")
+				{
+					if (movies[i].category == category)
+					{
+						Console.WriteLine(movies[i].title);
+					}
+				}
+				else if (category == "Horror")
+				{
+					if (movies[i].category == "Horror")
+					{
+						Console.WriteLine(movies[i].title);
+					}
+				}
+				else if (category == "Comedy")
+				{
+					if (movies[i].category == "Comedy")
+					{
+						Console.WriteLine(movies[i].title);
+					}
+				}
+				else if (category == "Drama")
+				{
+					if (movies[i].category == "Drama")
+					{
+						Console.WriteLine(movies[i].title);
+					}
+				}
+				else
+				{
+                    Console.WriteLine();
+					Console.WriteLine("Sorry, we dont have that here.");
+					break;
+				}
+			}
+		}
+	}
+}
---
+++ MovieLab/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using MovieLab;
+List<Movie> movieList = new List<Movie> {
+new Movie("Toy Story", "Animated"),
+new Movie("A Goofy Movie", "Animated"),
+new Movie("Basket Case", "Horror"),
+new Movie("Dead Alive", "Horror"),
+new Movie("My Bestfriends Girl", "Comedy"),
+new Movie("Shallow Hal", "Comedy"),
+new Movie("Hottub Time Machine", "Comedy"),
+new Movie("The Notebook", "Drama"),
+new Movie("Four Brothers", "Drama"),
+new Movie("Blow", "Drama"),
+};
+bool goAgain = true;
+Console.WriteLine("Welcome to the Movie List Application!");
+while (goAgain)
+{
+    Console.Write("Please pick a genre: \nHorror, Animated, Drama, or Comedy ");
+    string userInput = Console.ReadLine().ToLower().Trim();
+    List<Movie> movies = movieList.Where(x => x.Category.ToLower().Trim() == userInput).ToList();
+    if (movies.Count == 0)
+    {
+        Console.WriteLine("We dont have that genre.");
+    }
+    else
+    {
+        foreach (Movie m in movies)
+        {
+            Console.WriteLine(m.Title);
+        }
+    }
+    while (true)
+    {
+        Console.WriteLine("Would you like to pick another category? Please enter y/n");
+        string choice = Console.ReadLine().Trim().ToLower();
+        if (choice == "y")
+        {
+            break;
+        }
+        else if (choice == "n")
+        {
+            Console.WriteLine("Okay, see you next time!");
+            goAgain = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("invalid input..");
+        }
+    }
+}
